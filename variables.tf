5da4cea907c352f78e71c51f747279c460577dfd
AvinashBodhe1 <135799650+AvinashBodhe1@users.noreply.github.com>
2023-11-21 13:02:00 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -24,7 +24,6 @@ variable "subnet_id" {
 }
 
 variable "instance_id" { 
-    #default = "ocid1.instance.oc1.eu-zurich-1.an5heljr2f6evfqcve2bq6fgwv43retht2vk7lsooelmkui6wp22237mi3ma"
 }
 
 variable "session_display_name" {
@@ -32,7 +31,6 @@ variable "session_display_name" {
 }
 
 variable "session_key_details_public_key_content" {
-  #default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC9HLTG/GtjHeFkZ9TCY6we0rK68oqAd2WkUj181N8vpt5ejYG7Oz6NpR4H8NSI5Vpra9DpGZMJRcMD56hj8vl3fNDcZp9j9D4ZmIuNBJ6rqOTOspnLoknT/c+5HILMzSgvFVFBvVQ4ftM030ldMsaGuCIyo8O6b3eB5SxSvoXZBvupaKhhrfQGilrNuVqtt7GJNB6aVXPfL/DOhX0DXOxwq/FVRMH1ovIXgoeg0rzzX5q3KYGVq2ZNWFYpVzFihf7ecJyvQ1SYOF6okrN+bKq3iQ+oKcveAfvpjma+LEI+TrZEJ7Xd0A/b1gh4HJsSa+pKCP+MYqjkFkEE3zqoDWYB bastions"
 }
 
 variable "session_key_type" {
